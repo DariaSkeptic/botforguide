ac8bf44571a38e756ca270d5a83ba0553784fa02
Daria <foxponts@gmail.com>
2025-08-14 14:36:01 +0300

    Update where.py

--- where.py
+++ where.py
@@ -1,6 +1,10 @@
+import logging
 from telegram import Update
 from telegram.ext import ContextTypes
 
+logger = logging.getLogger(__name__)
+
 async def cmd_where(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.info("Команда /where вызвана")
     await update.message.reply_text("Ты находишься в боте для выдачи гайдов по арканам. "
                                     "Используй /start <код> для начала или /admin, если ты администратор.")
